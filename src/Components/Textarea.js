728d1a79aeee78ecb0d52cae791341384c3c80be
bhushanpatill <bhushanpatil8194@gmail.com>
2024-03-06 15:15:03 +0530

    Textbox and buttons are created

--- src/Components/Textarea.js
+++ src/Components/Textarea.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function Textarea(props) {
+
+    const onChangeFunction = (event) =>{
+        console.log("onchnage function occur")
+        setText(event.target.value);
+    }
+
+    const onClickUp = () =>{
+        console.log("Uppercase clicked");
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+    const onClickdown = () =>{
+      console.log("Lowercase clicked");
+      let newText = text.toLowerCase();
+      setText(newText);
+  }
+    const onClickRemove = () =>{
+      console.log("Remove clicked");
+      let newText = "";
+      setText(newText);
+  }
+
+  const [text, setText] = useState("Enter text here: ");
   return (
       <div className = "container my-3">
         <h2>{props.heading}</h2>
-        <textarea className="form-control my-3" id="exampleFormControlTextarea1" rows="10"></textarea>
+        <div>
+            <textarea className="form-control my-3" value = {text} onChange = {onChangeFunction} id="exampleFormControlTextarea1" rows="10"></textarea>
+        </div>
+        
+        <button className="btn btn-primary mx-3 " onClick = {onClickUp}>Convert Uppercase</button>
+        <button type="button" className="btn btn-warning mx - 3" onClick = {onClickdown}>Convert Lowercase</button>
+        <button type="button" className="btn btn-danger mx-3" onClick = {onClickRemove}>Remove Text</button>
         </div>
   )
 }
